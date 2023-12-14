e219a8e7c6950b902d4ee9a5a858783abefa434d
Stacey Vanyo <sbvanyo@gmail.com>
2023-12-13 19:27:06 -0600

    created post and user models

---
+++ rareapi/models/post.py
@@ -0,0 +1,12 @@
+from django.db import models
+from .user import User
+
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=50)
+    publication_date = models.DateField()
+    image_url = models.CharField(max_length=50)
+    content = models.CharField(max_length=50)
+    approved = models.BinaryField()
+    rare_user_id = models.ForeignKey(User, on_delete=CASCADE)
