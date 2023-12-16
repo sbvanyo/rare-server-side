702c573f6c8711f9895b6749e8f0f4278d7ca1e5
Olivia Daniels <danielsolivia4199@gmail.com>
2023-12-16 10:25:44 -0600

    4

--- rare/urls.py
+++ rare/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers
+
+router = routers.DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('checkuser', check_user),
 ]
