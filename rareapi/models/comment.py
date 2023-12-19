4fc479b619056b66a7277fa930379dd0c4fc6966
datcog1111 <h9tuck@gmail.com>
2023-12-18 21:29:47 -0600

    tags

--- rareapi/models/comment.py
+++ rareapi/models/comment.py
@@ -4,6 +4,6 @@ from .post import Post
 
 class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comment_posts")
     content = models.CharField(max_length=100)
     created_on = models.DateField(auto_now=False)
